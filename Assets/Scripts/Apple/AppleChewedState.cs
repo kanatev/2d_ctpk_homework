1b9a79f9e1a560a8b360c87d0184558faff9dd04
Alex Kanatev <45164382+kanatev@users.noreply.github.com>
2023-12-29 13:15:43 +0100

    Sound of collecting apples added
    Added:
     - Sound of collecting apples
     - a bit of refactoring

--- Assets/Scripts/Apple/AppleChewedState.cs
+++ Assets/Scripts/Apple/AppleChewedState.cs
@@ -24,6 +24,9 @@ public class AppleChewedState: AppleBaseStateAbstract
         int counter = int.Parse(scoreLabel.GetComponentsInChildren<TextMeshProUGUI>()[1].text);
         counter += 1;
         scoreLabel.GetComponentsInChildren<TextMeshProUGUI>()[1].text = counter.ToString();
+
+        // play sound
+        apple.GetComponent<AudioSource>().Play();
     }
 
     public override void UpdateState(AppleStateManagerContext apple)
